aa7c0ae8b945b932a9da2da4ebf9fce330c0f40b
Anastasiya <blabla@.mail.ru>
2025-02-24 21:29:49 +0300

    task 1 fixed

--- src/main/java/ru/learning/github/Main.java
+++ src/main/java/ru/learning/github/Main.java
@@ -2,7 +2,7 @@ package ru.learning.github;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println("Hello, world!");
         }
         }
